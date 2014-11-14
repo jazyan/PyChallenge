15f7ad9d768b00c10fd3b0cb00cb58d405f8890f
jasmineyan <jasmineyan@college.harvard.edu>
2014-11-14 14:27:20 -0500

    solved 3

--- prob3.py
+++ prob3.py
@@ -1,4 +1,11 @@
-f = open('prob3.in', 'r')
+import string, re
+f = open('prob3.in')
 message = f.read()
+f.close()
 
+message = message.replace("\n", "")
 
+r = re.compile("[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]")
+extract = r.findall(message)
+
+print ''.join(extract)
