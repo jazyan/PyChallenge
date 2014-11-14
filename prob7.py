8d5bb3538e7a97e8d1540b660f4f6f73bd413050
jasmineyan <jasmineyan@college.harvard.edu>
2014-11-14 16:34:49 -0500

    cleaned up 7

--- prob7.py
+++ prob7.py
@@ -9,12 +9,6 @@ for i in range(629):
     ans.append(pix[i, 47][0])
 
 ans = [chr(f) for f in ans]
-new = []
 
-for i in range(2, len(ans)-1):
-    if ans[i-1] != ans[i] and ans[i-1] != ans[i + 1]:
-        new.append(ans[i-1])
-
-print ''.join(new)
-
+ans = [ans[i] for i in range(len(ans)) if i % 7 == 0]
 print chr(105) + chr(110) + chr(116) + chr(101) + chr(103) + chr(114) + chr(105) + chr(116) + chr(121)
