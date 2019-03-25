a7a54f77538aa3e08c7cb935e4fa7e37f3c618a7
jasmine <jazyan11@gmail.com>
2019-03-24 22:26:25 -0700

    8 solution, and update 7 to be python3 friendly

--- prob7.py
+++ prob7.py
@@ -1,8 +1,8 @@
-import Image
+from PIL import Image
 
 im = Image.open("oxygen.png")
 pix = im.load()
-print im.size
+print(im.size)
 
 ans = []
 for i in range(629):
@@ -11,4 +11,4 @@ for i in range(629):
 ans = [chr(f) for f in ans]
 
 ans = [ans[i] for i in range(len(ans)) if i % 7 == 0]
-print chr(105) + chr(110) + chr(116) + chr(101) + chr(103) + chr(114) + chr(105) + chr(116) + chr(121)
+print(chr(105) + chr(110) + chr(116) + chr(101) + chr(103) + chr(114) + chr(105) + chr(116) + chr(121))
